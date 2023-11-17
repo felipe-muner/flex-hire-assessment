c0f47aa64d661051f9f65d773968b96a9b40ac62
Felipe Muner <felipe.muner@gmail.com>
2023-11-18 01:02:15 +0700

    feat: pass props with typescript interface

--- src/components/user-info/UserInfo.tsx
+++ src/components/user-info/UserInfo.tsx
@@ -1,9 +1,24 @@
-type UserInfoProps = {
-  currentUser: any;
-};
+export interface UserInfoProps {
+  currentUser: CurrentUser;
+}
+
+export interface Skill {
+  readonly name: string | null | undefined;
+}
+
+export interface UserSkill {
+  readonly skill: Skill | null | undefined;
+  readonly experience: number | null | undefined;
+}
+
+export interface CurrentUser {
+  name: string;
+  avatarUrl: string;
+  userSkills: readonly UserSkill[] | undefined;
+}
 
 function UserInfo(props: UserInfoProps) {
-  return <div>{JSON.stringify(props.currentUser)}</div>;
+  return <div>{JSON.stringify(props.currentUser.userSkills)}</div>;
 }
 
 export default UserInfo;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,17 +1,25 @@
 import { UserInfo } from "../components";
-import { usePreloadedQuery } from "react-relay";
+import { GraphQLTaggedNode, usePreloadedQuery } from "react-relay";
+import { AppQuery } from "../__generated__/AppQuery.graphql";
 
-function ProfilePage({
-  queryReference,
-  query,
-}: {
+interface ProfilePageProps {
   queryReference: any;
-  query: any;
-}) {
-  const data = usePreloadedQuery(query, queryReference);
-  console.log("data", data);
+  query: GraphQLTaggedNode;
+}
 
-  return <UserInfo currentUser={data} />;
+function ProfilePage(props: ProfilePageProps) {
+  const data = usePreloadedQuery<AppQuery>(props.query, props.queryReference);
+
+  return (
+    <UserInfo
+      currentUser={{
+        name: data.currentUser?.name ?? "",
+        avatarUrl: data.currentUser?.avatarUrl ?? "",
+        // pass skills as an empty array if it's null
+        userSkills: data.currentUser?.userSkills ?? [],
+      }}
+    />
+  );
 }
 
 export default ProfilePage;
