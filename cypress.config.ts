e2d142c9914d5de95beb60e4917b1d247716b895
Felipe Muner <felipe.muner@gmail.com>
2023-11-22 17:36:30 +0700

    feat: set https://flex-hire-assessment.vercel.app/profile

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "https://flex-hire-assessment.vercel.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
