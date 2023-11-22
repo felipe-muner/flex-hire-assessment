67d7d8a733c0029cf28664fb37183f355b8e5d46
Felipe Muner <felipe.muner@gmail.com>
2023-11-22 19:31:00 +0700

    chore: move message

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -2,11 +2,13 @@ import { Box, Typography } from "@mui/material";
 
 interface NotFoundProps {}
 
+const NOT_FOUND_MESSAGE = "Not Found, please contact the admin";
+
 function NotFound(props: NotFoundProps) {
   return (
     <Box style={{ marginTop: "20px" }}>
       <Typography gutterBottom variant="h4">
-        Not Found, please contact the admin
+        {NOT_FOUND_MESSAGE}
       </Typography>
     </Box>
   );
